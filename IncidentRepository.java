dffce7c2d118810c8a47e8de64c996a8bc15ebf9
Simar <115409175+simrbatra@users.noreply.github.com>
2025-04-26 21:53:34 +0530

    Create IncidentRepository.java

---
+++ IncidentRepository.java
@@ -0,0 +1,9 @@
+package com.example.incidentlog.repository;
+
+import com.example.incidentlog.model.Incident;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IncidentRepository extends JpaRepository<Incident, Long> {
+}
